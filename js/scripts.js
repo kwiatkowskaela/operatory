e2dd73b6f804b4f33921115194d1d0520776f303
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-08-03 23:13:23 +0200

    zadanie

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+var a = prompt('wpisz wartość a');
+var b = prompt('wpisz wartość b');
+var result = (a * a) + (2 * a * b) - (b * b);
+
+if (result > 0) {
+    alert('wynik dodatni');
+    console.log('wynik dodatni');
+} else if (result < 0) {
+    alert('wynik ujemny');
+    alert('wynik ujemny');
+    console.log('wynik ujemny');
+} else {
+    alert('wynik jest równy zero');
+    console.log('wynik jest równy zero');
+}
